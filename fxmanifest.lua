b9d8531658f764fa4aae4c74b790f53672a73a59
Kba <struys.wyvin@gmail.com>
2023-12-10 12:51:20 +0100

    Initialized commit

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Kba'
+description 'FiveM Loading Screen'
+version '1.0.0'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'css/style.css',
+    'js/script.js',
+}
+
+
+
